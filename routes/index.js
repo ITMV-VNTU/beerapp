2bdb8762415f46bab76e21fd91441f77fa094a12
vitalii <2kn15b.bondarchuk@example.com>
2017-11-18 19:10:12 +0200

    add all data

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var brewdb = new BreweryDb('d7d54b992833aa6dc7c1478cb6833f70');
 
 var fetch = require('node-fetch')
 var jsonfile = require('jsonfile')
-var file = './beers.json'
+var file = './routes/beers.json'
 
 
 /* GET home page. */
@@ -21,11 +21,20 @@ router.get('/', function(req, res, next) {
 
 
 });
+var json = require('json-file');
+var fs = require('fs');
+
+// Load a JSON file
+//var file = json.read('./beers.json');
+
 router.get('/getBeers', function (req,res,next) {
-    jsonfile.readFile(file, function(err, obj) {
-        console.log(obj)
-        res.render('getBeers',{data:obj})
-    })
+
+    var brewerie=JSON.parse(fs.readFileSync('./routes/breweries.json','utf8'))
+    var beer = JSON.parse(fs.readFileSync('./routes/beers.json', 'utf8'));
+    var categorie=JSON.parse(fs.readFileSync('./routes/categories.json','utf8'))
+    console.log(beer);
+    res.render('getBeers',{beers:beer.beers,breweries:brewerie,categories:categorie})
+
 })
 
 module.exports = router;
