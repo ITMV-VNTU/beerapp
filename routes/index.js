aab6db8ada50d945461516fc1cd19ff2edbf11a3
DereviankoMiroslav <derevianko_miroslav@ukr.net>
2017-11-18 16:50:41 +0200

    Beers Json

--- routes/index.js
+++ routes/index.js
@@ -15,21 +15,18 @@ router.get('/', function(req, res, next) {
     brewdb.style.all(function(error,data){
       beer=data;
         //console.log(data)
-        fetch('http://api.brewerydb.com/v2/breweries?key=d7d54b992833aa6dc7c1478cb6833f70',{
-            method:'get',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Origin': '',
-                'Host': 'api.producthunt.com'
-                }
-            }).then(function (response) {
+        fetch('http://api.brewerydb.com/v2/breweries?key=d7d54b992833aa6dc7c1478cb6833f70')
+        .then(function (response) {
             console.log(response)
             return response.json()
                 .then(function (data) {
                     console.log(data)
                 })
+                .catch((error) => console.log("Error parse:"+error));
         })
+        .catch(function(err){
+            console.log("Error fetch"+err);
+        });
         res.render('index', { data: beer });
     });
 
