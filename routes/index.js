9dc9b97a006a4cc9209538d648d3d09b1bd9cb22
vitalii <2kn15b.bondarchuk@example.com>
2017-11-19 00:23:00 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ var beer = null;
 var categorie = null;
 var newbeers = null;
 var countrys = null;
+var breweries_geocode=null;
 
 /* GET home page. */
 
@@ -34,13 +35,14 @@ router.get('/', function(req, res, next) {
     // /beer/:beerId
     brewerie = JSON.parse(fs.readFileSync('./routes/breweries.json','utf8'))
     beer = JSON.parse(fs.readFileSync('./routes/beers.json', 'utf8'));
-    categorie = JSON.parse(fs.readFileSync('./routes/categories.json','utf8'))
+    categorie = JSON.parse(fs.readFileSync('./routes/categories.json', 'utf8'));
+    breweries_geocode=JSON.parse(fs.readFileSync('./routes/breweries_geocode.json', 'utf8'));
     newbeers = logic.filterBeers(beer);
     countrys = logic.getCountrys(brewerie);
 
 
     res.render('index', { countries:countrys });
-    console.log(countrys)
+    //console.log(countrys)
 
 });
 var json = require('json-file');
@@ -53,7 +55,7 @@ router.get('/getBeers', function (req,res,next) {
     var brewery_id = req.query.brewery_id;
     if(brewery_id!=undefined||brewery_id!=null){
         var data = logic.filterBeersByBrewerie(newbeers, brewery_id);
-        console.log(data);
+        //console.log(data);
         res.render('getBeers',{beers:data,breweries:brewerie,categories:categorie})
     }
     else{
@@ -63,13 +65,16 @@ router.get('/getBeers', function (req,res,next) {
 
 router.get('/getBreweries', function (req,res,next) {
     var country = req.query.country;
-    console.log(country);
+    //console.log(country);
     if(country!=undefined){
         var data = logic.filterBreweriesByCountry(brewerie, country);
-        res.render('getBreweries',{breweries:data});
+        console.log(data)
+
+        res.render('getBreweries',{breweries:data,breweries_geocode:breweries_geocode});
     }
     else
-        res.render('getBreweries',{breweries:brewerie})
+        res.render('getBreweries',{breweries:brewerie,breweries_geocode:breweries_geocode})
 })
+
 
 module.exports = router;
